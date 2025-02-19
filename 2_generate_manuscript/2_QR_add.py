87b0bfab0891b09c76e7c582d0abc45a8a028f85
林家弘 <82699511+Garvin2009@users.noreply.github.com>
2025-02-19 14:33:42 +0800

    Update 2_QR_add.py

--- 2_generate_manuscript/2_QR_add.py
+++ 2_generate_manuscript/2_QR_add.py
@@ -6,8 +6,10 @@ import qrcode.image.svg
 from xml.etree import ElementTree as ET
 
 # 創建存儲 QR Code 的資料夾
-qrcode_folder = "./qrcode"
-os.makedirs(qrcode_folder, exist_ok=True)
+qrcode_folder1 = "./qrcode"
+os.makedirs(qrcode_folder1, exist_ok=True)
+qrcode_folder2 = "./Merge"
+os.makedirs(qrcode_folder2, exist_ok=True)
 
 for i in range(info.TOTAL_PAGES):
     # 讀取原本的 SVG 檔案
@@ -19,10 +21,10 @@ for i in range(info.TOTAL_PAGES):
     qr_code = qrcode.make(i+1, image_factory=factory)
 
     # 將 QR Code 存成檔案
-    qr_code.save(f"{qrcode_folder}/qrcode_{i+1:03d}.svg")
+    qr_code.save(f"{qrcode_folder1}/qrcode_{i+1:03d}.svg")
 
     # 讀取 QR Code SVG 檔案
-    with open(f"{qrcode_folder}/qrcode_{i+1:03d}.svg", "rb") as qr_file:
+    with open(f"{qrcode_folder1}/qrcode_{i+1:03d}.svg", "rb") as qr_file:
         qr_code_svg_str = qr_file.read().decode('utf-8')
 
     # 找到最後一個 </svg> 標籤的位置
